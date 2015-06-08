cbd435d0621ca553f790b1f7b9ad6d11da5ed805
Koen <koenkeune>
2015-06-08 03:01:44 +0200

    script to analyze the data

---
+++ scripts/dataset_analysis.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from scipy.stats.stats import pearsonr
+
+building = '740-NTH' #'761-KMH' #'882-WBW'
+data = building + '_elektriciteit_withWeather_clean.csv'
+path = os.path.dirname( __file__ )
+path = os.path.abspath(os.path.join(path, os.pardir))
+df = pd.read_csv(os.path.join(path, 'datasets/' + data).replace('\\', '/'))
+
+from datetime import date
+df['Momentum'] = df['Momentum'].astype('datetime64')
+df = df.set_index('Momentum', drop=False)
+
+# There is an implicit conversion of timezone. I need to fix it.
+df.index = df.index + pd.DateOffset(hours=1)
+del df['Momentum']
+df['Momentum'] = df.index
+
+# one year:
+df2 = df[(df.Momentum > date(2011, 01, 01)) & (df.Momentum < date(2012, 01, 01))]['gas [m3]']
+df2.plot(figsize=(15,7))
+plt.show()
+
+# one month:
+ax = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 03, 01))][['gas [m3]']].plot(figsize=(10,7), grid=False, legend=False, colormap='gray')
+ax.set_ylabel("Gas (m$^3$)")
+ax.set_xlabel("")
+plt.show()
+
+# compare with temperature:
+ax = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 03, 01))]['gas [m3]'].plot(figsize=(10,7), grid=False, legend=True, colormap='gray')
+ax = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 03, 01))]['T'].plot(figsize=(10,7), grid=False, colormap='gray', style='--', legend='T')
+plt.show()
+
+# one week:
+ax = df[(df.Momentum > date(2013, 02, 04)) & (df.Momentum < date(2013, 02, 11))]['gas [m3]'].plot(figsize=(10,7), grid=False, colormap='gray')
+ax.set_ylabel("Gas (m$^3$)")
+plt.show()
+
+# comparing two variables:
+x = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 04, 01))]['gas [m3]']
+y = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 04, 01))]['T']
+print 'pearson value and p-value between gas and temperature'
+print pearsonr(x.values, y.values)
+
+x = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 04, 01))]['gas [m3]']
+y = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 04, 01))]['Q']
+print 'pearson value and p-value between gas and global radiation'
+print pearsonr(x.values, y.values)
+
+x = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 04, 01))]['gas [m3]']
+y = df[(df.Momentum > date(2012, 02, 01)) & (df.Momentum < date(2012, 04, 01))]['elektriciteit [kwh]']
+print 'pearson value and p-value between gas and electricity'
+print pearsonr(x.values, y.values)
+
